dee8e4a8210f812a1a69c3643b90885ad9e454a5
bambusMalpa <you@example.combambusmalpa@gmail.com>
2019-07-07 23:13:25 +0200

    b

--- src/components/Pages/MyWorks.jsx
+++ src/components/Pages/MyWorks.jsx
@@ -26,28 +26,27 @@ const WorksWrapper=styled.section`
     width:80vw;
     display:grid;
     grid-template-columns:80vw;
-    grid-template-rows:80vw 80vw 80vw 80vw 80vw 80vw 80vw 80vw 80vw;
+    grid-template-rows:80vw 80vw 80vw 80vw 80vw 80vw 80vw 80vw;
     margin-left:auto;
     margin-right:auto;
     grid-gap: 1em;
     min-height:80vh;
     margin-bottom:10vh;
     @media(min-width:576px){
-        grid-template-rows:40vw 40vw 40vw 40vw 40vw;
+        grid-template-rows:40vw 40vw 40vw 40vw;
         grid-template-columns:40vw 40vw ;}
     @media(min-width:768px){
         grid-template-rows:25vw 25vw 25vw ;
         grid-template-columns:25vw 25vw 25vw;
         }
     @media(min-width:1200px){
-        grid-template-rows:40vh 40vh 40vh ;
+        grid-template-rows:40vh 40vh ;
         grid-template-columns:20vw 20vw 20vw 20vw ;
-        
+        margin-bottom:0vh;
     }
 `
 
 const projects=[{name:"Zegar binarny", link:"https://binaryclock.netlify.com/",github:"https://github.com/bambusmalpa/binary-clock",foto:image01,desc:"Moje początki z JS, zegar pokazujący godzinę w formie kodu binarnego",tech:[{color:"#e34f26",className:"fab fa-html5"},{color:"#0099e5",className:"fab fa-css3-alt"},{color:"#f7df1e",className:"fab fa-js"}]},
-{name:"Aplikacja pogodowa", link:"https://bambusmalpa.github.io/React-Weather-App/",github:"https://github.com/bambusmalpa/React-Weather-App",foto:image02,desc:`Pomysł na aplikację pochodzi z warsztatów "FrontDev", pobiera dane z API, generuje wykresy prognozy pogody dla wybranych miast`,tech:[{color:"#e34f26",className:"fab fa-html5"},{color:"#0099e5",className:"fab fa-css3-alt"},{color:"#f7df1e",className:"fab fa-js"},{color:"#9933CC",className:"fab fa-bootstrap"},{color:"#00d8ff",className:"fab fa-react"}]},
 {name:"Aplikacja pogodowa", link:"https://bambusmalpa.github.io/React-Weather-App/",github:"https://github.com/bambusmalpa/React-Weather-App",foto:image02,desc:`Pomysł na aplikację pochodzi z warsztatów "FrontDev", pobiera dane z API, generuje wykresy prognozy pogody dla wybranych miast`,tech:[{color:"#e34f26",className:"fab fa-html5"},{color:"#0099e5",className:"fab fa-css3-alt"},{color:"#f7df1e",className:"fab fa-js"},{color:"#9933CC",className:"fab fa-bootstrap"},{color:"#00d8ff",className:"fab fa-react"}]},
 {name:"Portfolio dla fotografa", link:"https://marcinpiotrowfoto.netlify.com/",github:"https://github.com/bambusmalpa/reactfotographerportfolio",foto:image03,desc:`Strona wykonana w React, z użyciem React Router. Strona w produkcji`,tech:[{color:"#e34f26",className:"fab fa-html5"},{color:"#0099e5",className:"fab fa-css3-alt"},{color:"#f7df1e",className:"fab fa-js"},{color:"#9933CC",className:"fab fa-bootstrap"},{color:"#00d8ff",className:"fab fa-react"}]},
 {name:"Nowy projekt logo", link:"https://mplogo.netlify.com/",github:"https://github.com/bambusmalpa/smirasowelogo",foto:image04,desc:`Przeprojektowane logo dla prywatnego fotografa`,tech:[{color:"#e34f26",className:"fab fa-html5"},{color:"#cc0000",className:"fas fa-file-image"}]},
