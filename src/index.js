26d70a96564b9cdde3427ee66ce663481768fab8
bambusMalpa <you@example.combambusmalpa@gmail.com>
2019-07-07 20:09:49 +0200

    remove adress bar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+window.addEventListener("load",function() {
+  setTimeout(function(){
+      // This hides the address bar:
+      window.scrollTo(0, 1);
+  }, 0);
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
